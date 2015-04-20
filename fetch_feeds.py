1cdc14648f7e621501411cec34bde0277a9b41b4
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2015-04-19 22:04:55 -0400

    Add argument to set verbosity

--- fetch_feeds.py
+++ fetch_feeds.py
@@ -12,19 +12,20 @@ from feed_sources import *
 
 logging.basicConfig()
 LOG = logging.getLogger()
-LOG.setLevel(logging.DEBUG)
+LOG.setLevel(logging.INFO)
 
 NJ_TRANSIT_CLASS = 'NJTransit'
 
 def fetch_all(get_nj, nj_username, nj_password, sources=None):
     """Fetch from all FeedSources in the feed_sources directory."""
-    LOG.info('Going to fetch feeds from sources: %s', sources)
     statuses = {}  # collect the statuses for all the files
 
     # make a copy of the list of all modules in feed_sources;
     # default to use all of them (excluding NJ, if not requested)
     if not sources:
         sources = list(feed_sources.__all__)
+
+    LOG.info('Going to fetch feeds from sources: %s', sources)
 
     # NJ TRANSIT requires some special handling; do that here
     if NJ_TRANSIT_CLASS in sources:
@@ -58,7 +59,9 @@ def fetch_all(get_nj, nj_username, nj_password, sources=None):
             LOG.error('Skipping feed %s, which could not be found.', src)
 
     # remove last check key set at top level of each status dictionary
-    del statuses['last_check']
+    if statuses.has_key('last_check'):
+        del statuses['last_check']
+
     for file_name in statuses:
         stat = statuses[file_name]
         if stat.has_key('error'):
@@ -84,8 +87,12 @@ def main():
                         help='Password for NJ TRANSIT developer account (optional)')
     parser.add_argument('--feeds',
                         help='Comma-separated list of feeds to get (optional; default: all)')
+    parser.add_argument('--verbose', '-v', action='count')
 
     args = parser.parse_args()
+    if args.verbose:
+        LOG.setLevel(logging.DEBUG)
+
     if args.get_nj and (not args.nj_username or not args.nj_password):
         LOG.warn('--nj-username and --nj-password are required to fetch NJ TRANSIT feeds.')
         sys.exit(1)
@@ -93,7 +100,6 @@ def main():
     # should specify --get-nj when email received saying new download available
     if args.feeds:
         sources = args.feeds.split(',')
-        LOG.debug('Running with sources %s.', sources)
         fetch_all(args.get_nj, args.nj_username, args.nj_password, sources=sources)
     else:
         fetch_all(args.get_nj, args.nj_username, args.nj_password)
