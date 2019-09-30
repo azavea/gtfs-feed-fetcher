2a37edaef3be06ab97bb1561b51d7977909e4abe
Kathryn Killebrew <kkillebrew@azavea.com>
2019-09-30 10:29:18 -0400

    Update CT Transit feed fetcher
    CT Transit has changed their URLs.

--- feed_sources/CTTransit.py
+++ feed_sources/CTTransit.py
@@ -5,24 +5,17 @@ from FeedSource import FeedSource
 
 LOG = logging.getLogger(__name__)
 
-BASE_URL = 'http://www.cttransit.com/uploads_GTFS/'
+BASE_URL = 'http://www.cttransit.com/sites/default/files/gtfs/googlect_transit.zip'
 SHORELINE_EAST_URL = 'http://www.shorelineeast.com/google_transit.zip'
+HARTFORD_URL = 'http://www.hartfordline.com/files/gtfs/gtfs.zip'
 
 
 class CTTransit(FeedSource):
     """Fetch PATH feed."""
     def __init__(self):
         super(CTTransit, self).__init__()
-        # feeds for Hartford, New Haven, Stamford, Waterbury, New Britain, Meridien,
-        # and Shore Line East.  Shore Line East has its own URL.
-        ct_suffixes = {'Hartford': 'ha', 'New Haven': 'nh', 'Stamford': 'stam',
-                       'Waterbury': 'wat', 'New Britain': 'nb', 'Meridien': 'me'}
-
-        urls = {}
-        for sfx in ct_suffixes:
-            url = '%sgoogle%s_transit.zip' % (BASE_URL, ct_suffixes[sfx])
-            filename = 'ct_%s.zip' % ct_suffixes[sfx]
-            urls[filename] = url
-
-        urls['ct_shoreline_east.zip'] = SHORELINE_EAST_URL
-        self.urls = urls
+        self.urls = {
+            'ct_transit.zip': BASE_URL,
+            'ct_shoreline_east.zip': SHORELINE_EAST_URL,
+            'ct_hartford_rail.zip': HARTFORD_URL
+        }
